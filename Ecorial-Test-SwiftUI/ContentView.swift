00d2c3984383ca1c253c5ca53d12accaca9250cc
Igor Palyvoda <tib.igor.mk@gmail.com>
2021-01-02 13:05:09 +0200

    Added headerView

--- Ecorial-Test-SwiftUI/ContentView.swift
+++ Ecorial-Test-SwiftUI/ContentView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            headerView
+        }
     }
 }
 
+extension ContentView {
+    var headerView: some View {
+        VStack(alignment: .center) {
+            HStack(spacing: 22) {
+                Spacer()
+                Text("My Memorials")
+                Spacer()
+                Button(action: {}) {
+                    Image.addIcon
+                }
+                .padding(.trailing, 10)
+            }
+            .padding(.leading, 50)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
