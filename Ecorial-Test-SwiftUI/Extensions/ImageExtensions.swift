2a016a6f85228793cf2ae064b5d32a04644871e4
Igor Palyvoda <tib.igor.mk@gmail.com>
2021-01-02 11:49:43 +0200

    Added images

---
+++ Ecorial-Test-SwiftUI/Extensions/ImageExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  ImageExtensions.swift
+//  Ecorial-Test-SwiftUI
+//
+//  Created by Ingvar on 02.01.2021.
+//
+
+import SwiftUI
+
+
+extension Image {
+
+    static var addIcon: Image = Image("addIcon")
+    static var backIcon: Image = Image("backIcon")
+    static var favIcon: Image = Image("favIcon")
+    
+}
