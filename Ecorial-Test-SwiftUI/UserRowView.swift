5447a15613b538424b22c5c96677866d64bcb733
Igor Palyvoda <tib.igor.mk@gmail.com>
2021-01-02 16:21:34 +0200

    Added ui for userRow

--- Ecorial-Test-SwiftUI/UserRowView.swift
+++ Ecorial-Test-SwiftUI/UserRowView.swift
@@ -8,26 +8,58 @@
 import SwiftUI
 
 struct UserRowView: View {
+    
     let user: UserInfo
     
     var body: some View {
         ZStack {
-            Image(user.image).resizable().edgesIgnoringSafeArea(.bottom)
-
-            BlurView(
-                effect: .systemMaterialDark)
-                .frame(height:100)
-                .padding()
+            Image(user.image).resizable().frame(height: 288)
+            descriptionView
         }
         .cornerRadius(10)
-        
+        .padding()
     }
 }
 
+extension UserRowView {
+    var descriptionView: some View {
+        ZStack {
+            BlurView(effect: .systemMaterialDark)
+                .frame(height:93)
+                .offset(y: 100)
+            userBio
+                .offset(y: 45)
+                .padding(.leading, -80)
+        }
+    }
+    
+    var userBio: some View {
+        VStack(alignment: .leading, spacing: 7) {
+            Image(user.userPhoto)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 22)
+                        .stroke(Color.white, lineWidth: 2)
+                )
+            HStack {
+                VStack(alignment: .leading, spacing: 2) {
+                    Text(user.name)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    Text(user.description)
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                    Text(DateFormatter.full.string(from: user.date))
+                        .font(.system(size: 12))
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+    }
+}
 
 struct UserRowView_Previews: PreviewProvider {
     static var previews: some View {
         UserRowView(user: UserInfo.fakeItem())
-            .previewLayout(.fixed(width: 347, height: 291))
+            .previewLayout(.fixed(width: 347, height: 288))
     }
 }
